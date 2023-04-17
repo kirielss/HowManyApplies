eab2eab92e4f881db3f3fec9f4f5402bfdadc14c
skiriel <gabrieldllopes@gmail.com>
2023-04-17 00:04:07 -0300

    feat: base applies count

---
+++ project-files/src/App.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    switch (count) {
+      case 1:
+        setMessage("Best of luck!");
+        break;
+
+      case (10):
+        setMessage("That many applications already? Hope you get an interview!");
+        break;
+
+      case (25):
+        setMessage("Don't give up! Your time will come!");
+        break;
+
+      case (50):
+        setMessage("I can see you are working hard and I am proud of you!");
+        break;
+
+      case (100):
+        setMessage("Frustrating, isn't it? But you can do it! Remember your dream...");
+        break;
+
+      case (250):
+        setMessage("We are proud of you for taking it so far. Soon you will get noticed");
+        break;
+
+      case (500):
+        setMessage("Never forget: you are a great developer! Your dream will come true!");
+        break;
+
+      case (1000):
+        setMessage("If only you knew how many people are rooting for you right now... I wish I could hire you myself, but since I can't, please remember this: You are not alone!");
+
+    }
+  })
+
+
+  return (
+    <div className="App">
+      <div className='card'>
+        <button onClick={() => setCount(count + 1)}>Applied? Good luck this time!</button>
+        <h1>How Many Applies?</h1>
+        <p>{count}</p>
+        <h2>{message}</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
